a13425d808ac42e77e3423718daa69cf1bad2b8a
Kyle Kirkland <kyle.kirkland@omicronmedia.com>
2017-06-27 12:27:22 -0400

    Updated

--- TimeTable/ViewController.swift
+++ TimeTable/ViewController.swift
@@ -38,7 +38,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 
+        return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
